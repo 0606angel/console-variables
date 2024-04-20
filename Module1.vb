cac308115bf0dc7102b230071a0a56881dd8129b
SIKHIPHA ANGEL <SIKHIPHA ANGEL@LUFUNO>
2024-04-20 23:56:56 +0200

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,33 @@
+﻿Option Strict On
+Option Explicit On
+
+Module Module1
+    Private colour As String = "grey"
+    Private flower As String = "Roses"
+    Private food As String = "pizza"
+    Private subject As String = "infomatics"
+    Private age As String = "20"
+    Private hobbies As String = "sleeping"
+
+    Sub Main()
+        Console.WriteLine("what is your favourite colour?" + colour)
+
+        Console.WriteLine("what are your favourite flowers? " + flower)
+        Console.WriteLine("what is your favourite food? " + food)
+        Console.WriteLine("what is your favourite subject? " + subject)
+        Console.WriteLine("How old are you? " + age)
+
+        Console.WriteLine("what is your favourite hobbies? " + hobbies)
+        Console.ReadKey()
+
+
+
+
+
+
+    End Sub
+
+
+
+End Module
+
